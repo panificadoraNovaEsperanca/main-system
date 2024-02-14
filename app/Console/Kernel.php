1d76073c8222f5d258eabd48019fbb62db93a26f
Heryck <heryckmota@gmail.com>
2024-02-14 12:32:59 -0300

    dfsdfsdsdfsdfa

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,6 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-       VerificaValidade::class,
     ];
 
     /**
@@ -27,7 +26,6 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('verifica:validade')->everyMinute()->sendOutputTo('storage/logs/laravel.log');
     }
 
     /**
