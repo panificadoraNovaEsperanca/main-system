8dc0195ac4770479279e1a99edfb4a4d7396ce8d
HeryckMbs <68446973+HeryckMbs@users.noreply.github.com>
2024-02-15 13:28:09 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+php artisan key:generate
+hp artisan storage:link
+npm i
+npm run build
+npm run dev
 php artisan migrate:fresh --seed --force
 php artisan serve --host 0.0.0.0 --port $APP_PORT
