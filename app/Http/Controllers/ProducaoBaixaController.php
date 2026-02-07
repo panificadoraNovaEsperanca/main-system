f605df10cadd8447a51d17820c1c1bc15a7a019a
Heryck Mota <heryckmota@gmail.com>
2026-02-07 11:03:47 -0300

    Update debug output in ProducaoBaixaController to log specific date range for production data retrieval, enhancing troubleshooting capabilities.

--- app/Http/Controllers/ProducaoBaixaController.php
+++ app/Http/Controllers/ProducaoBaixaController.php
@@ -28,10 +28,16 @@ class ProducaoBaixaController extends Controller
         $producaos = Producao::with(['produto', 'produto.categoria', 'user'])
             ->whereBetween('dt_inicio', [$inicio, $fim])
             ->paginate(request()->query('paginacao', 30));
-        dump(Carbon::now());
         $producaoCategoria = [];
-        dump(Producao::whereBetween('dt_inicio', [$inicio, $fim])->select(['produto_id','quantidade','status','dt_inicio','observacao','user_id'])->get());
-        dump(['inicio' => $inicio, 'fim' => $fim]);
+        dump(
+            Producao::whereBetween('dt_inicio', [
+                '2026-02-04 17:00:00',
+                '2026-02-04 19:04:00'
+            ])
+            ->select(['produto_id','quantidade','status','dt_inicio','observacao','user_id'])
+            ->get()
+        );
+                dump(['inicio' => $inicio, 'fim' => $fim]);
         dump($producaos->items());
         foreach ($producaos as $producao) {
             
